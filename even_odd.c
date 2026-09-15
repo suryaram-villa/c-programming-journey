0fbd671b9766ad66a7bd797e608ab7cab6df100d
Suryaram Akhilesh Villa <suryaram.villa@gmail.com>
2026-09-15 19:49:07 +0530

    Create even_odd.c

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter any no.:");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("It is even.");
+    }
+    else{
+        printf("It is odd.");
+    }
+}
